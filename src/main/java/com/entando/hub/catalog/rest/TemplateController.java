c14737d2fadc02a208456c8bc78e3c0fa134a9fc
daksu493 <deniz.a493@gmail.com>
2021-12-02 17:02:05 +0100

    Lista Order

--- src/main/java/com/entando/hub/catalog/rest/TemplateController.java
+++ src/main/java/com/entando/hub/catalog/rest/TemplateController.java
@@ -2,12 +2,15 @@ package com.entando.hub.catalog.rest;
 
 import lombok.Data;
 import org.springframework.web.bind.annotation.*;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.annotation.security.RolesAllowed;
 
 @RestController
-@RequestMapping("/api/example")
+@RequestMapping("/api")
 public class TemplateController {
+    /*
 
     @RolesAllowed("et-first-role")
     //@PreAuthorize("hasAuthority('ROLE_et-first-role')")
@@ -21,6 +24,22 @@ public class TemplateController {
     @Data
     public static class MyResponse{
         private final String payload;
+    }
+    */
+    
+    //@PreAuthorize("hasAuthority('ROLE_et-first-role')")
+    @CrossOrigin
+    @GetMapping("/order")
+    public List<Order> getOrders() {
+        return Arrays.asList(new Order[]{ new Order("test Data ", 1,"descrizione")});
+    }
+
+    @Data
+    public static class Order{
+        private final String name;
+        private final Integer id;
+        private final String description;
+
     }
 
 }
